2bf911d56859cfb8ca779e3307ea6b2a43c57d23
LTaeHoon <lth1011@gmail.com>
2016-12-13 11:16:26 +0900

    자바기초 - 객체지향언어

---
+++ chap01_Basic/src/basic/Step03_oop.java
@@ -0,0 +1,20 @@
+package basic;
+
+public class Step03_oop {
+	/*
+	 * java 프로그램 특징
+	 * 1. 겍체지향언어(java, c++)
+	 * 2. 클래스 단위로 프로그램 작성
+	 * 3. 한 문장은 세미클론으로 끝난다.
+	 * 4. 클래스 : 첫 자는 대문자, 메서드(함수): 소문자()
+	 * 5. main() : 프로그램 시작점을 의미하는 특수한 메서드
+	 */
+	public static void main(String[] args) {
+		// 프로그램 시작점
+		System.out.println("프로그램 시작");  // ln -> line skip
+		System.out.print("java ");
+		System.out.print("program");
+		System.out.println("\n프로그램 종료"); // \n : 줄바꿈 이탈 문자
+	}
+
+}
