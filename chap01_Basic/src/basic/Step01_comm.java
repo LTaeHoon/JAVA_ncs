ccfdd2a0032b730e33b5202b7aa8d4cbea8dc33c
LTaeHoon <lth1011@gmail.com>
2016-12-13 11:15:22 +0900

    자바기초 - 주석처리

---
+++ chap01_Basic/src/basic/Step01_comm.java
@@ -0,0 +1,26 @@
+package basic; //한 줄 주석 : package: 동일한 클래스의 모임(폴더 유사)
+
+/*
+ * 두 줄 이상 주석
+ * 작성자 : 홍길동
+ * 작성일 : 2016.12.13
+ * 내용: java 기초
+ */
+public class Step01_comm {
+	/*
+	 * Class : 파란(Step01_comm 이런것들이 클래스) , Method() : 초록색
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("자바 프로그래밍");
+		System.out.println("java programming");
+		/*
+		 * System.out : 표준출력장치(모니터,프린터)
+		 * println : 출력(line skip)
+		 * 자동완성 기능 단축키 : Ctrl + Space bar
+		 * 실행 단축키 : Ctrl + F11
+		 */
+	}
+
+}
