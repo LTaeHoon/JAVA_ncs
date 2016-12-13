5b5e169eee5da700248877aebeb8788ce8822c4b
LTaeHoon <lth1011@gmail.com>
2016-12-13 15:19:23 +0900

    자바첫번째 강의 과제

---
+++ homework/src/home/class03.java
@@ -0,0 +1,15 @@
+package home;
+
+public class class03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	   int fat, car,pro,cal;
+	   fat = Integer.parseInt(args[0]);
+	   car = Integer.parseInt(args[1]);
+	   pro = Integer.parseInt(args[2]);
+	   cal = fat*9 + pro*4 + car*4;
+	   System.out.printf("총칼로리: %d cal",cal);
+	}
+
+}
